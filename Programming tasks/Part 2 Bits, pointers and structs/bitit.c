891e012d030441f201536f70055a738882c8c9de
Aaro Karhu <152692647+Ar-Kah@users.noreply.github.com>
2025-03-06 16:50:24 +0200

    pass

--- Programming tasks/Part 2 Bits, pointers and structs/bitit.c
+++ Programming tasks/Part 2 Bits, pointers and structs/bitit.c
@@ -2,19 +2,64 @@
 #include <stdio.h>
 
 void scharBitit(signed char x) {
-    int bit;
-    printf("%d", x);
-    bit = (x >> i) & 1;
+    int bit, i;
+    int zeroes = 0;
+    int ones = 0;
+    printf("%d\n", x);
+    for (i = sizeof(signed char) * 8 - 1; i >= 0; i--) {
+        bit = (x >> i) & 1;
+        printf("%d", bit);
+        bit == 1 ? ones++ : zeroes++;
+    }
+    printf("\n");
+    printf("%d\n", zeroes);
+    printf("%d\n", ones);
 }
 
 void shortBitit(short int x) {
+    int bit, i;
+    int zeroes = 0;
+    int ones = 0;
+    printf("%d\n", x);
 
+    for (i = sizeof(short int) * 8 - 1; i >= 0; i--) {
+        bit = (x >> i) & 1;
+        printf("%d", bit);
+        bit == 1 ? ones++ : zeroes++;
+    }
+    printf("\n");
+    printf("%d\n", zeroes);
+    printf("%d\n", ones);
 }
 
 void intBitit(int x) {
+    int bit, i;
+    int zeroes = 0;
+    int ones = 0;
+    printf("%d\n", x);
 
+    for (i = sizeof(int) * 8 - 1; i >= 0; i--) {
+        bit = (x >> i) & 1;
+        printf("%d", bit);
+        bit == 1 ? ones++ : zeroes++;
+    }
+    printf("\n");
+    printf("%d\n", zeroes);
+    printf("%d\n", ones);
 }
 
 void longBitit(long int x) {
+    int bit, i;
+    int zeroes = 0;
+    int ones = 0;
+    printf("%ld\n", x);
 
+    for (i = sizeof(long int) * 8 - 1; i >= 0; i--) {
+        bit = (x >> i) & 1;
+        printf("%d", bit);
+        bit == 1 ? ones++ : zeroes++;
+    }
+    printf("\n");
+    printf("%d\n", zeroes);
+    printf("%d\n", ones);
 }
