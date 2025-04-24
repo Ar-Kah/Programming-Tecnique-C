4096e856f92cf13abf212ff1511e6d68a47e1131
Aaro Karhu <152692647+Ar-Kah@users.noreply.github.com>
2025-04-24 13:43:31 +0300

    fibo

--- Programming tasks/Part 3 Dycanic memory handling/fibonacci.c
+++ Programming tasks/Part 3 Dycanic memory handling/fibonacci.c
@@ -1,22 +1,67 @@
 #include "fibonacci.h"
 #include <stdlib.h>
+#include <stdio.h>
 
-unsigned int *fib_jono(unsigned int n)
-{
-    int i;
-    unsigned int *ptr;
+char *fib_jono_mj(unsigned int n) {
+    int cap, used, m;
+    unsigned int a, b, temp, i, num;
+    char *string, *new_string;
 
-    ptr = (unsigned int*)malloc(n * sizeof(unsigned int));
-
-    for (i = 0; i < n; i++) {
-        if (i == 0) {
-            ptr[i] = 0;
-        } else if (i == 1) {
-            ptr[i] = 1;
-        } else {
-            ptr[i] = ptr[i-1] + ptr[i-2];
-        }
+    if (n == 0) {
+        char *empty_str = malloc(1);
+        if (!empty_str) return NULL;
+        empty_str[0] = '\0';
+        return empty_str;
     }
 
-    return ptr;
+    cap = 1;
+    used = 1;
+    string = malloc(cap);
+    if (!string) return NULL;
+    string[0] = '\0';
+
+    a = 0;
+    b = 1;
+
+    m = 1;
+    while (used + m > cap) cap *= 2;
+    new_string = realloc(string, cap);
+    if (!new_string) {
+        free(string);
+        return NULL;
+    }
+    string = new_string;
+    sprintf(string, "%u", a);
+    used += m;
+
+    for (i = 1; i < n; i++) {
+        temp = a + b;
+        a = b;
+        b = temp;
+
+        m = 2;
+        num = a;
+        while (num > 0) { 
+            m++; 
+            num /= 10; 
+        }
+
+        while (used + m > cap) cap *= 2;
+        new_string = realloc(string, cap);
+        if (!new_string) {
+            free(string);
+            return NULL;
+        }
+        string = new_string;
+
+        sprintf(string + used - 1, ", %u", a);
+        used += m;
+    }
+
+    new_string = realloc(string, used);
+    if (!new_string) {
+        free(string);
+        return NULL;
+    }
+    return new_string;
 }
--- Programming tasks/Part 3 Dycanic memory handling/fibonacci.h
+++ Programming tasks/Part 3 Dycanic memory handling/fibonacci.h
@@ -1,6 +1,6 @@
 #ifndef FIBONACCI_H
 #define FIBONACCI_H
 
-unsigned int *fib_jono(unsigned int n);
+char *fib_jono_mj(unsigned int n);
 
 #endif
